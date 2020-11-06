378b1a57595ac909b6477fb9924443512351503d
yingliufengpeng <2906631584@qq.com>
2020-11-06 10:04:18 +0800

    添加 Guessing Game 添加幸运号number

--- Guessing_game/src/main.rs
+++ Guessing_game/src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Please input your guess");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input you guess.");
 
     let mut guess = String::new();
 
