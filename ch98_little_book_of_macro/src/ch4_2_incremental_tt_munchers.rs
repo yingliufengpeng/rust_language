340fcd505c1af42dbc9d6153385cec8da0e4a5a1
yingliufengpeng <2906631584@qq.com>
2020-11-13 15:46:15 +0800

    ch4.2Incremental TT Munchers

--- ch98_little_book_of_macro/src/ch4_2_incremental_tt_munchers.rs
+++ ch98_little_book_of_macro/src/ch4_2_incremental_tt_munchers.rs
@@ -1,9 +1,43 @@
 
+
+macro_rules! mixed_rules {
+
+    () => {};
+
+    (trace $name:ident; $($tail:tt)*) => { {
+        println!(concat!(stringify!($name), " = {:?}"), $name);
+        mixed_rules!($($tail)*);
+    }};
+
+    (trace $name:ident = $init:expr; $($tail:tt)* ) => {{
+        let $name = $init;
+        println!(concat!(stringify!($name), " = {:?}"), $name);
+        mixed_rules!($($tail)*);
+
+    }};
+}
+
+macro_rules! KK {
+    ($name:ident) => {
+       {
+            println!(concat!(stringify!($name), "  xxx   ", "end"));
+       }
+    }
+}
+
+// It is important, howevr, to keep the macro recursion limit in mid,
+// macro_rules! does not have any form of tail recursion or optimisation. It is
+// recommended that, when writing a TT muncher, you make reasonable efforts to
+// keep recursion as limited as possible.
+
 #[cfg(test)]
 mod tests {
 
     #[test]
     fn test_001() {
+        let x = 44;
+        let y = 40;
+        mixed_rules!(trace x ; trace y; trace z = 30;);
 
     }
 
