3cd9364e81a21b9fbba6cfc85aa8c4d7a9cf4e95
yingliufengpeng <2906631584@qq.com>
2020-11-13 15:07:07 +0800

    ch4.1 Callbacks

--- ch98_little_book_of_macro/src/ch4_1_callbacks.rs
+++ ch98_little_book_of_macro/src/ch4_1_callbacks.rs
@@ -1,12 +1,41 @@
 
+macro_rules! call_with_larch {
+    ($callback:ident) => { $callback!(larch)  };
+}
 
+macro_rules! expand_to_larch {
+    () => { larch };
+}
+
+
+macro_rules! recognise_tree {
+    (larch) => { println!("#1, the Larch.") };
+    (redwood) => { println!("#2, the Mighty Redwood.") };
+    (fir) => { println!("#3, the Fir.") };
+    (chestnut) => { println!("#4, the Horse Chestnut.") };
+    (pine) => { println!("#5, the Scots Pine.") };
+    ($($other:tt)*) => { println!("I don't know; some kind of birch maybe?") };
+
+}
+
+
+macro_rules! callback {
+    ($callback:ident($($args:tt)*)) => {
+        $callback!( $($args)* )
+    };
+
+    () => {println!("end");}
+}
 
 #[cfg(test)]
 mod tests {
 
     #[test]
     fn test_001() {
+        recognise_tree!(expand_to_larch!());
+        call_with_larch!(recognise_tree);
 
+        callback!(callback(callback(println("ok")) ));
     }
 
 }
