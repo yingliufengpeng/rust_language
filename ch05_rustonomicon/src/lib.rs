1f4926b326c29b5c9ff527d35733e998c48c9d86
yingliufengpeng <2906631584@qq.com>
2020-11-19 15:23:28 +0800

    ch0.0 Introduction

---
+++ ch05_rustonomicon/src/lib.rs
@@ -0,0 +1,16 @@
+//!
+//! The Dark Arts of Unsafe Rust.
+//!
+
+
+pub mod section00_introduction;
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
