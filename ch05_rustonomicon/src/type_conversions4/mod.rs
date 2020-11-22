b086e989b9a196ea05472e5a761aed1dedb11f6c
yingliufengpeng <2906631584@qq.com>
2020-11-22 10:34:35 +0800

    ch4.4 Transmutes 实践为主

--- ch05_rustonomicon/src/type_conversions4/mod.rs
+++ ch05_rustonomicon/src/type_conversions4/mod.rs
@@ -9,6 +9,7 @@
 pub mod section4_1_coercions;
 pub mod section4_2_the_dot_operator;
 pub mod section4_3_casts;
+pub mod section4_3_transmutes;
 
 #[cfg(test)]
 mod tests {
---
+++ ch05_rustonomicon/src/type_conversions4/section4_3_transmutes.rs
@@ -0,0 +1,22 @@
+
+//!
+//!
+//! # Transmutes
+//!     Get out of our way type systems! We're going to
+//!         reinterpret these bits or die trying!.
+//!
+
+
+
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_001() {
+
+
+
+    }
+
+}
